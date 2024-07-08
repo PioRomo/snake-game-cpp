98dd2429bdf201ef0eb2d06416c5c9b88d8a5c31
PioRomo <promocolleges@gmail.com>
2024-07-08 12:13:11 -0700

    Game loop created

--- main.cpp
+++ main.cpp
@@ -3,7 +3,26 @@
 
 using namespace std; 
 
+Color teal = {0, 109, 117, 255};
+Color orange = {234, 114, 0, 255};
+
+int cellSize = 25; 
+int cellCount = 20; 
+
 int main(){
 
+    InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake Game C++");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false){
+        BeginDrawing();
+
+        //Drawing
+        ClearBackground(teal);
+
+        EndDrawing(); 
+    }
+
+    CloseWindow(); 
     return 0; 
 }
